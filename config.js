cdc79252625296092cc30b0fa13f94bfc77842ad
bob yasoo <matador200499@gmail.com>
2026-08-02 19:00:33 +0000

    feat: تحويل إلى توليد فيديوهات تلقائية - Python + FFmpeg + Edge TTS

--- config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const config = {
-  geminiApiKey: process.env.GEMINI_API_KEY,
-  facebookPageId: process.env.FACEBOOK_PAGE_ID,
-  facebookPageAccessToken: process.env.FACEBOOK_PAGE_ACCESS_TOKEN,
-};
